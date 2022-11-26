d71993e887e8d2336e6e7597a527f7b88d981d63
vientoeste <djjd2000@naver.com>
2022-11-26 16:16:15 +0900

    Implemented Authorization

--- app.js
+++ app.js
@@ -1,7 +1,46 @@
 import * as express from 'express';
-import { ChildProcess, spawn } from 'child_process';
+import { spawn } from 'child_process';
+import { config } from 'dotenv';
+import session from 'express-session';
+import passport from 'passport';
+
+config();
+
+import { router as authRouter } from '../routes/auth.js';
 
 const app = express.default();
+passportConfig();
+
+app.set('view engine', 'njk');
+
+nunjucks.configure('views', {
+  express: app,
+  watch: true,
+});
+const sessionMiddleware = session({
+  resave: false,
+  saveUninitialized: false,
+  secret: process.env.COOKIE_SECRET,
+  cookie: {
+    httpOnly: true,
+    secure: false,
+  },
+});
+
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'uploads')));
+app.use(express.static(path.join(__dirname, 'views')))
+app.use('/views', express.static(path.join(__dirname + 'views')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(sessionMiddleware);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/auth', authRouter);
 
 app.get('/exampleRouter', (req, res) => {
     const process = spawn('php', ["./ex.php", req.query.query]);
--- ex.php
+++ ex.php
@@ -1,2 +1,7 @@
 <?php
-echo $argv[1];
+$mysql = mysqli_connect($argv[1]->$HOST, $argv[1]->$ID, $argv[1]->$PW, $argv[1]->$DB);
+if (mysqli_connect_errno($mysql)){
+    echo "DB 연결 실패:" . mysqli_connect_error(); 
+}else{
+    echo "DB 연결 성공" ;
+}
